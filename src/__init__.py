16812db4d2289a82a22ceef92297e5be5558b9bd
viyas52 <vyas.illusionist@gmail.com>
2024-07-18 22:46:21 +0530

    custom logger

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,8 @@ LOG_FILE_PATH=os.path.join(logs_dir,LOG_FILE)
 
 logging.basicConfig(
     
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(name)s , line : %(lineno)d - %(levelname)s - %(message)s",
+    datefmt="%d-%m-%Y %I:%M:%S %p",
     level=logging.INFO,
     handlers=[
         logging.FileHandler(LOG_FILE_PATH),
