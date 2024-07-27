b72efeda156444096185f01cda34552cd4f94be7
viyas52 <vyas.illusionist@gmail.com>
2024-07-27 23:39:02 +0530

    web page with flask

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
-from cnnClassifier.utils import decodeImage
-from cnnClassifier.pipeline.predict import DogCat
+from src.utils import decodeImage
+from src.pipeline.predict import DogCat
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
@@ -21,3 +21,25 @@ class ClientApp:
 @cross_origin()
 def home():
     return render_template('index.html')
+
+
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    os.system("python main.py")
+    return "Training done successfully!"
+
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predictiondogcat()
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host='0.0.0.0', port=8080)
