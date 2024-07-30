49067946533b8feddfa3c3d9da32a93b8123d4c4
viyas52 <vyas.illusionist@gmail.com>
2024-07-30 13:36:52 +0530

    predict commit

--- src/pipeline/predict.py
+++ src/pipeline/predict.py
@@ -1,6 +1,6 @@
 import numpy as np 
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model # type: ignore
+from tensorflow.keras.preprocessing import image # type: ignore
 import os
 
 
